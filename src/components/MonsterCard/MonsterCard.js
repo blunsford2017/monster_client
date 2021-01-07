b13ef485cf744c33666a348f46f3e2aa476f350b
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-07 09:43:48 -0800

    Adding basic components and temporary styling.

---
+++ src/components/MonsterCard/MonsterCard.js
@@ -0,0 +1,11 @@
+import './MonsterCard.css';
+
+const MonsterCard = (props) => {
+    return (
+        <div className="monster-card">
+            <p> This is a monster card. </p>
+        </div>
+    );
+}
+
+export default MonsterCard;
